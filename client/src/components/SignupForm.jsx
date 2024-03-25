3231430a0dff8cad8fcbd491be0f3881681cde65
Kevin Pierce <kpierce236@gmail.com>
2024-03-25 17:27:16 -0400

    Cleaned SignupForm

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -13,7 +13,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-  const [addUser] = useMutation(ADD_USER);
+  const [addUser, {error, data}] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
