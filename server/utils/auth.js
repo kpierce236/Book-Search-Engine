6f59a16441a11f9d7ff9bc7d909e0f6aef242976
Kevin Pierce <kpierce236@gmail.com>
2024-03-25 18:14:18 -0400

    added mongoDB dependency

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
+const secret = process.env.SECRET_SESSION;
 const expiration = '2h';
 
 module.exports = {
