76a09fc1a9d19d015f60d6535e13b0203cb7d7f9
Kevin Pierce <kpierce236@gmail.com>
2024-03-25 11:50:32 -0400

    Changed the auth middelware in the sever file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
+const { expressMiddleware } = require('@apollo/server/express4');
 const db = require('./config/connection');
 const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
@@ -12,32 +13,31 @@ const PORT = process.env.PORT || 3011;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => {
-    const context = { req }; // Pass req to the context object
-    return authMiddleware(context); // Call your authentication middleware and pass the context
-  },
 });
 
 async function startApolloServer() {
 
   await server.start();
-  server.applyMiddleware({ app });
   
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
 
-app.use(routes);
+  app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
+  db.once('open', () => {
+    app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
