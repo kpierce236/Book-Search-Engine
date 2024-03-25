3491ca494786a0e1c167d9bcdb635ec00d813b70
Kevin Pierce <kpierce236@gmail.com>
2024-03-24 20:04:09 -0400

    added apollo server instance

--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3011;
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+      // Add your authentication middleware to the context
+      authMiddleware({ req });
+      return { user: req.user }; // Pass the authenticated user to context
+    },
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+}
+
+startApolloServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.use(routes);
